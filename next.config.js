ce241ec44ad028662ec493c2348fbb1a463324fa
Hitesh-Attri <hiteshattri71@gmail.com>
2023-08-19 03:07:12 +0530

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Set Cache-Control header to prevent caching for API routes
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store",
+          },
+        ],
+      },
+      {
+        // Set Cache-Control header for other routes (adjust as needed)
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
